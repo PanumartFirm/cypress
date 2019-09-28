2712217d2fe999c8ea3ef7f18eaec379ba60125e
PanumartFirm <pspisozsw2@hotmail.com>
2019-09-28 15:49:40 +0700

    first commit

---
+++ cypress/integration/sample.spec.js
@@ -0,0 +1,13 @@
+describe('Cypress Getting Started',() => {
+    it('Does not do much thing ',()=>{
+        cy.visit('http://example.cypress.io')
+        cy.contains('type').click()
+        cy.url().should('include','commands/actions')
+        cy.get('.action-email')
+        .type('fake@gmail.com')
+        .should('have.value','fake@gmail.com')
+        cy.get('.action-div').dbclick().should('not.be.visible')
+        cy.get('.action-input-hidden').should('be.visible')
+
+    })
+})
