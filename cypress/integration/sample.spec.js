3f77f6d8ed1af49b3ebaef4209061bb495ef7a12
PanumartFirm <pspisozsw2@hotmail.com>
2019-09-28 15:53:41 +0700

    commit

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -6,7 +6,8 @@ describe('Cypress Getting Started',() => {
         cy.get('.action-email')
         .type('fake@gmail.com')
         .should('have.value','fake@gmail.com')
-        cy.get('.action-div').dbclick().should('not.be.visible')
+
+        cy.get('.action-div').dblclick().should('not.be.visible')
         cy.get('.action-input-hidden').should('be.visible')
 
     })
